89ede9b33de8c202cb36b1d1d7aac1c652efd661
YJH16120 <yapjiahong2003@gmail.com>
2021-04-23 21:00:37 +0800

    Added `cd` command, and `exit`, and error handling.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Alfred
+The butler you need! Alfred is a simple terminal emulator written in Rust.
+
+# Motivation
+Made it just for fun, plus I like shells.
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,53 @@
-use std::io;
+use std::io::{Write, stdin, stdout};
+
+use std::env;
+use std::path::Path;
 use std::process::Command;
-use std::io::Write;
 
 fn main() {
-    print!("> ");
-    let mut input = String::new();
-    io::stdout().flush().expect("Unabel to flush.");
+    loop {
+        print!("> ");
+        let mut input = String::new();
+        
+        // This is required to show output from the `print!` macro. If this isn't
+        // included this output will be shown during termination.
+        stdout().flush().expect("Unable to flush.");
 
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Unable to read line.");
+        stdin()
+            .read_line(&mut input)
+            .expect("Unable to read line.");
 
-    let command = input.trim();
+        // This creates a struct called SplitWhitespace
+        let mut main_command = input.trim().split_whitespace();
 
-    Command::new(command)
-        .spawn()
-        .expect("Invalid command.");
+        // A weird property of `next` is that on the first call it returns the
+        // first element in a collection.
+        let command = main_command.next().expect("End of iterator.");
 
-    let mut child = Command::new(command)
-        .spawn()
-        .expect("Command invalid.");
+        // `next` also "removes" the viewed element from the collection,
+        // so all that's left is everything after the viewed element
+        let args = main_command;
 
-    child.wait().expect("Process not waited on.");
+        // Some commands have to chnage the interals of a shell
+        // And therefore must be implemented manually. An example is the
+        // cd (change directory) command
+        match command {
+            "cd" => {
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            },
+            "exit" => return,
+            command => {
+                let mut child = Command::new(command)
+                    .args(args)
+                    .spawn()
+                    .expect("Command invalid.");
+
+                child.wait().expect("Process not waited on.");
+            }
+        };
+    }
 }
